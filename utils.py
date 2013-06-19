9f69f53c372b7b4a955a6196dccfe3c74f6769c3
Sandeva Goonetilleke <sandeva@gmail.com>
2013-06-19 10:50:20 +0900

    GAE login_required

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponsePermanentRedirect
+from google.appengine.api import users
+
 import re
 def getHtmlPath(module,html):
     r=re.sub(r'\.','/',module)
@@ -8,3 +11,13 @@ def getParentPath(module):
     r=re.sub(r'\.','/',"/"+module)
     r=re.sub("[^/]*$",'',r)
     return r
+
+def login_required(func):
+    def redirect_to_login(request):
+        return HttpResponsePermanentRedirect(users.create_login_url(request.get_full_path()))
+
+    user = users.get_current_user()
+    if user:
+        return func
+    else:
+        return redirect_to_login
